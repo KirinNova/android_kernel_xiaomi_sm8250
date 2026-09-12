4b69ee889486a3cadd9dbcbfaf5fb63857e604b6
KirinNova <3058098928@qq.com>
2026-09-12 18:55:06 +0800

    fix

--- build_kernel.sh
+++ build_kernel.sh
@@ -249,7 +249,7 @@ if [ "$ENABLE_KSU" -eq 1 ]; then
     echo " [*] Initializing KernelSU  Setup"
     echo "==========================================="
     echo "[*] Downloading and running KernelSU remote setup script..."
-    curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
+    curl -LSs https://raw.githubusercontent.com/xiziya/SukiSU_Non-GKI/builtin/kernel/setup.sh | sh
     echo "[+] KernelSU setup finished."
 fi
 
